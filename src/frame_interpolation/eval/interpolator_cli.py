85f7ead60d7f34c14a204cb22e59135a94653dc6
David Marx <david.marx84@gmail.com>
2022-03-07 11:24:13 -0800

    added a bunch of logging to cli for debugging

--- src/frame_interpolation/eval/interpolator_cli.py
+++ src/frame_interpolation/eval/interpolator_cli.py
@@ -77,6 +77,7 @@ import natsort
 import numpy as np
 import tensorflow as tf
 
+from loguru import logger
 
 _PATTERN = flags.DEFINE_string(
     name='pattern',
@@ -143,11 +144,24 @@ class ProcessDirectory(beam.DoFn):
       media.set_ffmpeg(ffmpeg_path)
 
   def process(self, directory: str):
-    input_frames_list = [
-        natsort.natsorted(tf.io.gfile.glob(f'{directory}/*.{ext}'))
-        for ext in _INPUT_EXT
-    ]
+    logger.debug(directory)
+    
+    #input_frames_list = [
+    #    natsort.natsorted(tf.io.gfile.glob(glob_pat))
+    #    for ext in _INPUT_EXT
+    #]
+    input_frames_list = []
+    for ext in _INPUT_EXT:
+        glob_pat = f'{directory}/*.{ext}'
+        logger.debug(glob_pat)
+        found_frames = tf.io.gfile.glob(glob_pat)
+        logger.debug(found_frames)
+        found_frames = natsort.natsorted(found_frames)
+        logger.debug(found_frames)
+        input_frames_list.append(found_frames)
+    logger.debug(input_frames_list)
     input_frames = functools.reduce(lambda x, y: x + y, input_frames_list)
+    logger.debug(input_frames)
     logging.info('Generating in-between frames for %s.', directory)
     frames = list(
         util.interpolate_recursively_from_files(
@@ -160,6 +174,7 @@ class ProcessDirectory(beam.DoFn):
 
 def _run_pipeline() -> None:
   directories = tf.io.gfile.glob(_PATTERN.value)
+  logger.debug(directories)
   pipeline = beam.Pipeline('DirectRunner')
   (pipeline | 'Create directory names' >> beam.Create(directories)  # pylint: disable=expression-not-assigned
    | 'Process directories' >> beam.ParDo(ProcessDirectory()))
